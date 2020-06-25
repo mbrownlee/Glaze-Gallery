16ad29e91b21160104f034f2e89fc2c16d1bcddf
mbrownlee <michelle@orbwave.com>
2020-06-25 18:50:58 -0500

    randomized gallery view

--- src/components/pots/PotList.js
+++ src/components/pots/PotList.js
@@ -1,47 +1,46 @@
 import React, { useState, useEffect } from "react";
 import PotCard from "./PotCard";
-import {
-  Row,
-} from "reactstrap";
+import { Row } from "reactstrap";
 import API from "../../modules/fetch";
 
 const PotList = (props) => {
   const [pots, setPots] = useState([]);
-  
-  useEffect(() => {
-    
-  }, [pots]);
+
+  useEffect(() => {}, [pots]);
 
   const getPots = () => {
-  
     return API.getAllPots().then((potsFromAPI) => {
-      console.log(potsFromAPI, "potsfromApi")
+      function shuffle(potsFromAPI) {
+        let currentIndex = potsFromAPI.length,
+          temporaryValue,
+          randomIndex;
+        while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = potsFromAPI[currentIndex];
+          potsFromAPI[currentIndex] = potsFromAPI[randomIndex];
+          potsFromAPI[randomIndex] = temporaryValue;
+        }
+        return potsFromAPI;
+      }
+      shuffle(potsFromAPI);
       setPots(potsFromAPI);
     });
   };
-  
+
   useEffect(() => {
     getPots();
   }, []);
 
   return (
     <>
-    
       <div className="container-cards">
-      <Row sm="4">
-        {pots.map((pot) => (
-         
-          <PotCard
-            key={pot.id}
-            pot={pot}
-            {...props}
-          />
-          
-        ))}
+        <Row sm="4">
+          {pots.map((pot) => (
+            <PotCard key={pot.id} pot={pot} {...props} />
+          ))}
         </Row>
       </div>
-      
- 
     </>
   );
 };
